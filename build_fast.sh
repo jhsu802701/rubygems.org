ebd52444d836f3225b1ebe1c6fd1116e707d2b15
Jason Hsu <jhsu802701@jasonhsu.com>
2019-03-31 21:27:20 +0000

    Sets up PostgreSQL; successfully runs migrations

--- build_fast.sh
+++ build_fast.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sh pg_setup.sh
+
 sh kill_spring.sh
 sh all.sh
--- test_app.sh
+++ test_app.sh
@@ -6,3 +6,10 @@ echo '--------------'
 echo 'bundle install'
 bundle install
 
+echo '------------------------------------------------'
+echo 'RAILS_ENV=test bundle exec rake db:migrate:reset'
+RAILS_ENV=test bundle exec rake db:migrate:reset
+
+echo '--------------------------------------------------------------------'
+echo 'RAILS_ENV=test bundle exec rake db:create db:migrate db:test:prepare'
+RAILS_ENV=test bundle exec rake db:create db:migrate db:test:prepare
