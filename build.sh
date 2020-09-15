a1525f99ecc9fe2bb756f53e26615e8d1b87175e
Jason Hsu <jhsu802701@jasonhsu.com>
2020-09-15 20:26:38 +0000

    Added more steps to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,40 @@
 #!/bin/bash
 
-rvm install `cat .ruby-version`
+RUBY_VERSION=`cat .ruby-version`
+BUNDLER_VERSION='2.1.4'
+
+echo '-------------------------'
+echo "rvm install $RUBY_VERSION"
+rvm install $RUBY_VERSION
 wait
-rvm use `cat .ruby-version`
+
+echo '---------------------'
+echo "rvm use $RUBY_VERSION"
+rvm use $RUBY_VERSION
 wait
-gem install bundler -v 2.1.4
+
+echo '-------'
+echo 'ruby -v'
+ruby -v
+
+echo '---------------------------------------'
+echo "gem install bundler -v $BUNDLER_VERSION"
+gem install bundler -v $BUNDLER_VERSION
+wait
+
+echo '--------------'
+echo 'bundle install'
+bundle install
+wait
+
+echo '--------------'
+echo './script/setup'
+./script/setup
+
+echo '-----------------------------'
+echo './script/install_toxiproxy.sh'
+./script/install_toxiproxy.sh
+
+echo '-------------------------'
+echo 'bundle exec rake db:setup'
+bundle exec rake db:setup
